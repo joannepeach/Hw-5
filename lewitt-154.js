82cec19d8febded7bffd78c3bdc2fc1ad6b10d27
joannepeach <jlee20@cca.edu>
2017-02-22 03:57:56 -0800

    Create lewitt-154.js

---
+++ lewitt-154.js
@@ -0,0 +1,14 @@
+function setup() { 
+  createCanvas(600, 600);
+  
+  var rectH = 450;
+  var rectW = 530;
+  var rectX = 40;
+  var rectY = 80;
+  
+  rect(rectX, rectY, rectW, rectH);
+	stroke(348, 100, 100, 90);
+	line (450, 295, 40, 295);
+	
+	
+} 
